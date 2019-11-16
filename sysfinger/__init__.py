2e50c7426b1c0c567e7219963a574e2c8597fe88
psytron <m@psytron.com>
2019-11-16 03:45:51 -0800

    add basic

--- sysfinger/__init__.py
+++ sysfinger/__init__.py
@@ -4,3 +4,31 @@
 
 def run():
     print(' YES SYSFINGER ')
+
+import platform as p
+import uuid
+import hashlib
+
+def basic( md5=False ):
+    """
+    Fingerprint of the current operating system/platform.
+    If md5 is True, a digital fingerprint is returned.
+    """
+    sb = []
+    sb.append(p.node())
+    sb.append(p.architecture()[0])
+    sb.append(p.architecture()[1])
+    sb.append(p.machine())
+    sb.append(p.processor())
+    sb.append(p.system())
+    sb.append(str(uuid.getnode())) # MAC address
+    text = '_'.join(sb)
+    if md5:
+        md5 = hashlib.md5()
+        md5.update(text)
+        return md5.hexdigest()
+    else:
+        return text
+
+
+
