fa65036944603fb09945376e88c6ca305f7f84ad
psytron <m@psytron.com>
2020-04-09 21:44:28 +0100

    update basic finger

--- sysfinger/__init__.py
+++ sysfinger/__init__.py
@@ -10,26 +10,16 @@ import uuid
 import hashlib
 #import netifaces
 
-def basic( md5=False ):
-    """
-    Fingerprint of the current operating system/platform.
-    If md5 is True, a digital fingerprint is returned.
-    """
+def basic():
     sb = []
     sb.append(p.node())
-    sb.append(p.architecture()[0])
-    sb.append(p.architecture()[1])
+    sb.append( ''.join([ x for x in p.architecture() ]) )
     sb.append(p.machine())
     sb.append(p.processor())
     sb.append(p.system())
     sb.append(str(uuid.getnode())) # MAC address
-    text = '_'.join(sb)
-    if md5:
-        md5 = hashlib.md5()
-        md5.update(text)
-        return md5.hexdigest().encode('utf8')
-    else:
-        return text
+    text = '.'.join(sb)
+    return text
 
 
 def eth0():
@@ -38,3 +28,10 @@ def eth0():
     #return netifaces.ifaddresses('eth0')[netifaces.AF_LINK]
 
 
+def basic_md5():
+    unhashed =basic()
+    #    md5 = hashlib.md5().encode('utf8')
+    #md5.update(text)
+    #return md5.hexdigest().encode('utf8')
+    # hash here
+    return unhashed
